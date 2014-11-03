c49735ffd327354b54ff7f5c28bcb2baaccbd6ef
mrjadaml <mrjadaml@gmail.com>
2014-11-03 08:53:21 -0700

    created sessions controller, sign-in view, and handles invalid submissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   resources :artist_profiles, :users
 
-  get 'users/new'
-
-  root "pages#home"
-
-  get "about" =>    "pages#about"
-  get "contact" =>  "pages#contact"
+  root    "pages#home"
+  get     'users/new'
+  get     "about"      => "pages#about"
+  get     "contact"    => "pages#contact"
+  get     "login"      => "sessions#new"
+  post    "login"      => "sessions#create"
+  delete  "logout"     => "sessions#destroy"
 end
