3225ad373bdfd158a8d4ae725b40f2dfa3f87449
mrjadaml <mrjadaml@gmail.com>
2014-11-07 20:15:37 -0700

    Eliminated i18n::invalidlocaledata carrierwave bug

--- app/controllers/street_galleries_controller.rb
+++ app/controllers/street_galleries_controller.rb
@@ -9,11 +9,18 @@ class StreetGalleriesController < ApplicationController
   end
 
   def create
-    @street_gallery = StreetGallery.new(params.require(:street_gallery).permit(:image_mural, :active))
+    @street_gallery = StreetGallery.new(gallery_params)
     if @street_gallery.save
       redirect_to street_galleries_path
     else
       render :new
     end
   end
+
+  private
+
+  def gallery_params
+    params.require(:street_gallery).permit(:mural_image, :mural_image_cache, :active)
+  end
+
 end
