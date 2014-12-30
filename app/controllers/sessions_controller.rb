5321947f1a91090c52aa9c57d8a65d3da76a2206
mrjadaml <mrjadaml@gmail.com>
2014-12-29 20:06:12 -0700

    omniauth gem and config added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,20 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+
+
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
 
   root    'pages#home'
-  
+
   resources :murals, :users
 
-  get     'foo'               => 'users#foo'
-  get     'claim_profile'     => 'users#claim_profile'
-  get     'signup'            => 'registrations#new'
-  post    'signup'            => 'registrations#create'
-  get     'about'             => 'pages#about'
-  get     'contact'           => 'pages#contact'
-  get     'faq'               => 'pages#faq'
-  get     'login'             => 'sessions#new'
-  post    'login'             => 'sessions#create'
-  get     'logout'            => 'sessions#destroy'
+  get     'foo'                        => 'users#foo'
+  get     'claim_profile'              => 'users#claim_profile'
+  get     'signup'                     => 'registrations#new'
+  post    'signup'                     => 'registrations#create'
+  get     'about'                      => 'pages#about'
+  get     'contact'                    => 'pages#contact'
+  get     'faq'                        => 'pages#faq'
+  get     'login'                      => 'sessions#new'
+  get     '/auth/:provider/callback'   => 'sessions#create'
+  post    'login'                      => 'sessions#create'
+  get     'logout'                     => 'sessions#destroy'
 end
