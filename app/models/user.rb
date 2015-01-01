e84fa8751c211834c0dab3f24234e876ccd05c62
mrjadaml <mrjadaml@gmail.com>
2015-01-01 12:07:31 -0700

    dynamically set social provider handel on auth create. Turned off validations for email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ActiveRecord::Base
   has_many :authentications
   has_many :frames
   has_many :murals, through: :frames
-
-  before_save { self.email = email.downcase if email}
+  #
+  # if self.email?
+  #   before_save { self.email = email.downcase}
+  #   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #   validates :email, format: { with: VALID_EMAIL_REGEX },
+  #             uniqueness: { case_sensitive: false }
+  # end
   validates :first_name, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
 
