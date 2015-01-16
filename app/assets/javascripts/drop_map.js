1c7045338d6e77812ac4b5296c47763a07652f35
mrjadaml <mrjadaml@gmail.com>
2015-01-16 16:24:55 -0700

    click on group image links with form fields. Neet to pull in data objects to get specific vals. view restyled.

--- app/assets/javascripts/drop_map.js
+++ app/assets/javascripts/drop_map.js
@@ -55,13 +55,53 @@ var dropMap = function() {
   };
 
   google.maps.event.addListener(marker, 'dragend', function (event) {
-    $('.stream').empty();
+    $('.gallery').empty();
+    $('.gallery').append('<div class="mural-group"></div>');
     $.getJSON( "/groups", radius(), function(data) {
       data.forEach(function (x) {
-        var div = document.createElement('div');
-        $('.stream').append("<div class='col-md-3 gallery-mural" + x.id + "'>");
-        $('.gallery-mural' + x.id).append("<img src='" + x.image.user_ablum.url + "'>");
+        var muralImg = "<div data-mural-id='" + x.id + "' class='col-md-4 gallery-mural " + x.id + "'><img src='" + x.image.user_ablum.url + "'></div>";
+        $('.gallery').append(muralImg);
+      });
+      $( '.gallery-mural' ).on( 'click', function() {
+        var muralId = $( this ).data( 'mural-id' );
+        $( '.new_mural' ).append( '<input type="hidden" value="1" name="mural[group_id]" id="group_id">' ).val( muralId );
+        $( '#mural_user_id' ).val( 43 );
+        $( '#lat' ).val( "clicked mural lat value" );
+        $( '#long' ).val( "clicked mural lng value" );
+        $( '#mural_buffed' ).val( 0 );
       });
     });
   });
+
+  //
+  // <form class="new_mural" id="new_mural" enctype="multipart/form-data" action="/murals" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="9jB9T9qSFxPNJdHwFAXCQ+slBVhjNHhxCWFmnxMfl0JfivLYJHK9PrUIGykJUuiH+IPOdfTjFEKFJ2p4DiRssQ==">  <div class="form-group">
+  //
+  // <select class="form-control" name="mural[user_id]" id="mural_user_id"><option value="">Pick an artist</option>
+  // <option value="49">Keno Gonzales</option>
+  // <option value="43">Cannon Dill</option>
+  // <option value="4">Loyal</option>
+  //
+  // <input id="lat" class="form-control" type="hidden" name="mural[latitude]" value="39.747980045883544">
+  // <input id="long" class="form-control" type="hidden" name="mural[longitude]" value="-104.98282749311522">
+  //
+  // <input name="mural[buffed]" type="hidden" value="0">
+  // <input type="hidden" value="1" name="mural[group_id]" id="group_id">
+  //
+
+
+
+
+
+
+
+  // Click on pop-up image will work like a 'Submit'
+  // The click 'Submit' will envoke a confirmation of intention.
+  // Params taken from pop-up => :group_id, :artist(s), :latitude, :longitude, :buffed
+  // Params taken from user => :image, :user_id, :created_at :updated_at
+  // Generate a POST request with all the above params
+
+
+  // $(this).data('mural-id'))
+  // form .append(data fields)
+
 };
