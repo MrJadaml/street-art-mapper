47aa3cddfeabd694af5c6ec2cbdcbe420c64f427
mrjadaml <mrjadaml@gmail.com>
2015-01-20 18:17:20 -0700

    fades and boxshadows for drop map images. Placeholder data-types holding org lat and lng

--- app/assets/javascripts/drop_map.js
+++ app/assets/javascripts/drop_map.js
@@ -1,6 +1,6 @@
 var dropMap = function() {
   var myOptions = {
-    zoom: 14,
+    zoom: 15,
     center: myLatlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
@@ -53,9 +53,10 @@ var dropMap = function() {
     return bounds
   };
 
+  // Ajax call for murals within radius
   google.maps.event.addListener(marker, 'dragend', function (event) {
-    $('.gallery').empty();
-    $('.gallery').append('<div class="mural-group"></div>');
+    $('.near-by').empty();
+    $('.near-by').append('<div class="mural-group"></div>');
     $.getJSON( '/groups', radius(), function(data) {
       data.forEach(function (x) {
         var dataId = "data-mural-id='" + x.id + "'",
@@ -64,20 +65,46 @@ var dropMap = function() {
             dataArtist = "data-artist-id='" + x.user_id + "'",
             dataBuff = "data-buff='" + x.buffed + "'",
             classGMID =  " class='col-md-4 gallery-mural " + x.id,
-            imgSrc = "<img src='" + x.image.user_ablum.url + "'>";
-        var muralImg = "<div " + dataId + dataLat + dataLng + dataArtist + dataBuff + classGMID + "'>" + imgSrc + "</div>";
-        $('.gallery').append(muralImg);
-      });
-      $( '.gallery-mural' ).on( 'click', function() {
-        var muralId = $( this ).data( 'mural-id' ),
-            artist = $( this ).data('artist-id')
-        $( '.new_mural' ).append( '<input type="hidden" value="1" name="mural[group_id]" id="group_id">' ).val( muralId );
-        $( '#mural_user_id' ).val( artist );
-        $( '#lat' ).data( "lat" );
-        $( '#long' ).data( "lng" );
-        $( '#mural_buffed' ).data( 'buff' );
+            imgSrc = "<img src='" + x.image.user_ablum.url + "'>",
+            muralImg = "<div " + dataId + dataLat + dataLng + dataArtist + dataBuff + classGMID + "'>" + imgSrc + "</div>";
+        $('.near-by').append(muralImg);
       });
 
+      var addNew = '<div class="img-placeholder"><div class="dash-border"><div class="plus">+</div><div class="text">Add New</div></div></div>';
+      $( '.near-by' ).append( addNew );
+
+      $('.img-placeholder' ).on( 'click', function() {
+        $( this ).siblings().fadeTo( 'fast' , 0.2);
+        $( '.hide-me' ).show();
+        $( '.gallery-mural' ).children('img').removeClass('highlight')
+        $(this).attr( "data-latHolder", document.getElementById('lat').value );
+        $(this).attr( "data-lngHolder", document.getElementById('long').value );
+        $('#lat').val($( '.img-placeholder' ).data('latholder'));
+        $('#long').val($( '.img-placeholder' ).data('lngholder'));
+      });
+
+      // Autofill form data on click event from ajax rendered images.
+      $( '.gallery-mural' ).on( 'click', function() {
+        var muralId = $( this ).data( 'mural-id' ),
+            artist = $( this ).data('artist-id');
+        $(this).fadeTo( 'slow', 1 ).siblings().fadeTo( 'slow' , 0.5);
+        $( '.hide-me' ).hide();
+        $( '.new_mural' ).append( '<input type="hidden" value="1" name="mural[group_id]" id="group_id">' ).val( muralId );
+        $( '#mural_user_id' ).val( artist );
+        $( '#lat' ).val( $(this).data( 'lat' ));
+        $( '#long' ).val( $(this).data( 'lng' ));
+        $( '.gallery-mural' ).children('img').removeClass('highlight')
+        $(this).children('img').addClass('highlight')
+        $( '#mural_buffed' ).val( $(this).data( 'buff' ));
+      });
+
+      $( '.hide-me' ).on( 'click', function() {
+        // $( '.gallery-mural' ).fadeTo( 'slow', 1 );
+        $( '#mural_user_id' ).val( "" );
+        $('#lat').val($( '.img-placeholder' ).data('latholder'));
+        $('#long').val($( '.img-placeholder' ).data('lngholder'));
+        $( '#mural_buffed' ).val( "false" );
+      });
     });
   });
 };
