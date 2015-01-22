5ee0d209982d5754ed7368ce533ba36bb4a5cbe8
mrjadaml <mrjadaml@gmail.com>
2015-01-22 08:31:50 -0700

    set navbar buffers for views. Removed user(artist) validations on mural upload until I can find a better solution.

--- app/models/mural.rb
+++ app/models/mural.rb
@@ -2,7 +2,7 @@ class Mural < ActiveRecord::Base
   has_many :frames
   has_many :users, through: :frames
   default_scope -> { order('created_at DESC') }
-  validates :user_id, :image, :latitude, :longitude, presence: true
+  validates :image, :latitude, :longitude, presence: true
 
   mount_uploader :image, MuralUploader
 
