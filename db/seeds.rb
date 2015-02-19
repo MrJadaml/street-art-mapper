a0575f020e2147ab7bf4a1515cd982dc817dc85c
mrjadaml <mrjadaml@gmail.com>
2015-02-19 13:57:01 -0700

    added seed file for about 50 denver artist

---
+++ db/seeds.rb
@@ -0,0 +1,715 @@
+# User.create(
+#   first_name: "",
+#   last_name: "",
+#   twitter: "",
+#   instagram: "",
+#   artist: true,
+# )
+#
+# Authentication.create(
+#   user_id: ,
+#   provider: "",
+#   uid: "",
+# )
+
+
+alex = User.create(
+  first_name: "Alex",
+  last_name: "Hornest",
+  instagram: "item72forcollectors",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: alex.id,
+  provider: "instagram",
+  uid: "993904255",
+)
+
+anthoney = User.create(
+  first_name: "Anthoney",
+  instagram: "birdseedanthonysr",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: anthoney.id,
+  provider: "instagram",
+  uid: "383356622",
+)
+
+eine = User.create(
+  first_name: "Ben",
+  last_name: "Eine",
+  instagram: "einesigns",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: eine.id,
+  provider: "instagram",
+  uid: "einesigns",
+)
+
+berk = User.create(
+  first_name: "Berk",
+  twitter: "berkvisual",
+  instagram: "berkvisual",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: berk.id,
+  provider: "twitter",
+  uid: "22705739",
+)
+
+Authentication.create(
+  user_id: berk.id,
+  provider: "instagram",
+  uid: "31657395",
+)
+
+bunny = User.create(
+  first_name: "Bunny M",
+  instagram: "thebunnym",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: bunny.id,
+  provider: "instagram",
+  uid: "44178941",
+)
+
+star = User.create(
+  first_name: "Christina",
+  last_name: "Angelina",
+  twitter: "StarFighterA",
+  instagram: "starfightera",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: star.id,
+  provider: "twitter",
+  uid: "459744743",
+)
+
+Authentication.create(
+  user_id: star.id,
+  provider: "instagram",
+  uid: "52626736",
+)
+
+ethos = User.create(
+  first_name: "Claudio",
+  last_name: "Ethos",
+  instagram: "claudioethos",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: ethos.id,
+  provider: "instagram",
+  uid: "1389491601",
+)
+
+cls = User.create(
+  first_name: "CLS",
+  twitter: "",
+  instagram: "",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: cls.id,
+  provider: "instagram",
+  uid: "215898671",
+)
+
+choe = User.create(
+  first_name: "David",
+  last_name: "Choe",
+  twitter: "davidchoe",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: choe.id,
+  provider: "twitter",
+  uid: "239188281",
+)
+
+sheets = User.create(
+  first_name: "David",
+  last_name: "Sheets",
+  instagram: "davidsheets",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: sheets.id,
+  provider: "instagram",
+  uid: "256943047",
+)
+
+dread = User.create(
+  first_name: "Dread",
+  instagram: "dreadicrkd",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: dread.id,
+  provider: "instagram",
+  uid: "22603197",
+)
+
+east = User.create(
+  first_name: "East",
+  instagram: "east_este",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: east.id,
+  provider: "instagram",
+  uid: "32668190",
+)
+
+mac = User.create(
+  first_name: "El Mac",
+  twitter: "MAC_ARTE",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: mac.id,
+  provider: "twitter",
+  uid: "240114480",
+)
+
+dac = User.create(
+  first_name: "Fin",
+  last_name: "Dac",
+  twitter: "findac",
+  instagram: "findac",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: dac.id,
+  provider: "instagram",
+  uid: "34691649",
+)
+
+Authentication.create(
+  user_id: dac.id,
+  provider: "twitter",
+  uid: "73487424",
+)
+
+gamma = User.create(
+  first_name: "Gamma",
+  twitter: "gammagallery",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: gamma.id,
+  provider: "twitter",
+  uid: "82790469",
+)
+
+User.create(
+  first_name: "Gatz",
+  artist: true,
+)
+
+harleqin = User.create(
+  first_name: "Harleqin",
+  last_name: "Studio",
+  twitter: "harleqvinstudio",
+  instagram: "harleqvin_studio",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: harleqin.id,
+  provider: "twitter",
+  uid: "25379125",
+)
+
+Authentication.create(
+  user_id: harleqin.id,
+  provider: "instagram",
+  uid: "8153386",
+)
+
+hl = User.create(
+  first_name: "Hollis + Lana",
+  artist: true,
+)
+
+lm = User.create(
+  first_name: "LikeMindedArt",
+  twitter: "LikeMindedArt",
+  instagram: "likemindedart",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: lm.id,
+  provider: "instagram",
+  uid: "144414773",
+)
+
+Authentication.create(
+  user_id: lm.id,
+  provider: "twitter",
+  uid: "48437879",
+)
+
+molina = User.create(
+  first_name: "Jaime",
+  last_name: "Molina",
+  instagram: "cuttyup",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: molina.id,
+  provider: "instagram",
+  uid: "264172232",
+)
+
+jolt = User.create(
+  first_name: "Jolt",
+  last_name: "",
+  instagram: "guerillagarden",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: jolt.id,
+  provider: "instagram",
+  uid: "383281408",
+)
+
+joseph = User.create(
+  first_name: "Joseph",
+  last_name: "Martinez",
+  twitter: "JOSEPHMRTNZ",
+  instagram: "josephmrtnz",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: joseph.id,
+  provider: "twitter",
+  uid: "25348425",
+)
+
+Authentication.create(
+  user_id: joseph.id,
+  provider: "instagram",
+  uid: "1962551",
+)
+
+lux = User.create(
+  first_name: "Lux",
+  instagram: "ladylucx",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: lux.id,
+  provider: "instagram",
+  uid: "391482472",
+)
+
+max = User.create(
+  first_name: "Max",
+  last_name: "Kauffman",
+  twitter: "wittybanterism",
+  instagram: "wittybanterism",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: max.id,
+  provider: "twitter",
+  uid: "15927805",
+)
+
+Authentication.create(
+  user_id: max.id,
+  provider: "instagram",
+  uid: "35817766",
+)
+
+mear = User.create(
+  first_name: "Mear",
+  twitter: "mearone",
+  instagram: "mear_one",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: mear.id,
+  provider: "instagram",
+  uid: "53611243",
+)
+
+Authentication.create(
+  user_id: mear.id,
+  provider: "twitter",
+  uid: "77367297",
+)
+
+roane = User.create(
+  first_name: "Mike",
+  last_name: "Roane",
+  instagram: "mikeroane",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: roane.id,
+  provider: "instagram",
+  uid: "3158149",
+)
+
+nice = User.create(
+  first_name: "NiceOne",
+  instagram: "niceone101",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: nice.id,
+  provider: "instagram",
+  uid: "186320924",
+)
+
+pages = User.create(
+  first_name: "Pages",
+  instagram: "84pages",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: pages.id,
+  provider: "instagram",
+  uid: "236264210",
+)
+
+pedro = User.create(
+  first_name: "Pedro",
+  last_name: "Barrios",
+  instagram: "pbarriosart",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: pedro.id,
+  provider: "instagram",
+  uid: "284995477",
+)
+
+pj = User.create(
+  first_name: "PJ",
+  last_name: "Sierra",
+  twitter: "staleart",
+  instagram: "staleart303",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: pj.id,
+  provider: "instagram",
+  uid: "217727407",
+)
+
+Authentication.create(
+  user_id: pj.id,
+  provider: "twitter",
+  uid: "1306360015",
+)
+
+plant = User.create(
+  first_name: "Plaant",
+  twitter: "aNewerToday",
+  instagram: "plaant",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: plant.id,
+  provider: "instagram",
+  uid: "5587075",
+)
+
+Authentication.create(
+  user_id: plant.id,
+  provider: "twitter",
+  uid: "298852283",
+)
+
+swoon = User.create(
+  first_name: "Swoon",
+  twitter: "swoon",
+  instagram: "swoonhq",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: swoon.id,
+  provider: "twitter",
+  uid: "325183092",
+)
+
+Authentication.create(
+  user_id: swoon.id,
+  provider: "instagram",
+  uid: "281000738",
+)
+
+pew = User.create(
+  first_name: "Tony",
+  last_name: "dePew",
+  twitter: "pewey",
+  instagram: "tonydepew_",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: pew.id,
+  provider: "twitter",
+  uid: "21335766",
+)
+
+Authentication.create(
+  user_id: pew.id,
+  provider: "instagram",
+  uid: "184270081",
+)
+
+bimmer = User.create(
+  first_name: "Bimmer",
+  last_name: "Torrez",
+  twitter: "Bimmer_T",
+  instagram: "Bimmer_t",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: bimmer.id,
+  provider: "instagram",
+  uid: "204282066",
+)
+
+Authentication.create(
+  user_id: bimmer.id,
+  provider: "twitter",
+  uid: "426918393",
+)
+
+ying = User.create(
+  first_name: "YING",
+  twitter: "YourNameInGraff",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: ying.id,
+  provider: "twitter",
+  uid: "156547859",
+)
+
+eaks = User.create(
+  first_name: "MrEakso",
+  instagram: "mreakso",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: eaks.id,
+  provider: "instagram",
+  uid: "56041100",
+)
+
+sqr = User.create(
+  first_name: "SQUARE",
+  instagram: "s_q_u_a_r_e",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: sqr.id,
+  provider: "instagram",
+  uid: "54308082",
+)
+
+cool = User.create(
+  first_name: "SlowCoolAssault",
+  instagram: "slowcoolassault",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: cool.id,
+  provider: "instagram",
+  uid: "211558114",
+)
+
+brett = User.create(
+  first_name: "Brett",
+  last_name: "Flanigan",
+  instagram: "brettflanigan",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: brett.id,
+  provider: "instagram",
+  uid: "8479284",
+)
+
+cannon = User.create(
+  first_name: "CannonDill",
+  instagram: "cannondill",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: cannon.id,
+  provider: "instagram",
+  uid: "192447966",
+)
+
+sign = User.create(
+  first_name: "Signtologist",
+  twitter: "THESIGNTOLOGIST",
+  instagram: "thesigntologist",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: sign.id,
+  provider: "instagram",
+  uid: "1589991",
+)
+
+Authentication.create(
+  user_id: sign.id,
+  provider: "twitter",
+  uid: "19741270",
+)
+
+grope = User.create(
+  first_name: "TheGropes",
+  twitter: "thegropes",
+  instagram: "thegropes",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: grope.id,
+  provider: "twitter",
+  uid: "2391437972",
+)
+
+Authentication.create(
+  user_id: grope.id,
+  provider: "instagram",
+  uid: "309719689",
+)
+
+stay = User.create(
+  first_name: "StayAware",
+  instagram: "stayaware",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: stay.id,
+  provider: "instagram",
+  uid: "213759697",
+)
+
+lefavor = User.create(
+  first_name: "Scott",
+  last_name: "leFavor",
+  instagram: "scotlefavor",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: lefavor.id,
+  provider: "instagram",
+  uid: "20233955",
+)
+
+sandi = User.create(
+  first_name: "Sandi",
+  last_name: "Calistro",
+  instagram: "sandicalistro",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: sandi.id,
+  provider: "instagram",
+  uid: "5984315",
+)
+
+zoot = User.create(
+  first_name: "Mario",
+  last_name: "Zoots",
+  twitter: "MarioZoots",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: zoot.id,
+  provider: "twitter",
+  uid: "34992314",
+)
+
+lawn = User.create(
+  first_name: "ForestLawn",
+  twitter: "theforestlawn",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: lawn.id,
+  provider: "twitter",
+  uid: "185726819",
+)
+
+stain = User.create(
+  first_name: "RoyalStain",
+  twitter: "RoyalStainDSN",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: stain.id,
+  provider: "twitter",
+  uid: "237600743",
+)
+
+deep = User.create(
+  first_name: "Hari + Deepti",
+  instagram: "harianddeepti",
+  artist: true,
+)
+
+Authentication.create(
+  user_id: deep.id,
+  provider: "instagram",
+  uid: "1196867672",
+)
